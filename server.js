b8687fb4149772d89a6ce135c8c2a1cabd429356
SEP5155 <serg.papusha@gmail.com>
2024-11-24 22:46:39 +0200

    adjusted code to work on prod

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.all('*', (req, res, next) => {
     next();
 })
 
-app.listen('*', () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log('server is listening now')
 })
